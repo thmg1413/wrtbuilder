cbf0ba83c033d361656ba87bdd09fcbbdb72602c
thmg1413 <82623565+thmg1413@users.noreply.github.com>
2026-07-12 11:51:30 +0800

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,3 +6,7 @@ wget -O - https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/fee
 wget -O /usr/bin/libcronet.so \
   "https://github.com/SagerNet/cronet-go/releases/latest/download/libcronet-linux-arm64.so" \
 && chmod 0755 /usr/bin/libcronet.so
+1. 执行你之前的插件拉取脚本（UPDATE_PACKAGE克隆argon主题）
+2. 执行本脚本
+3. ./scripts/feeds update -a && ./scripts/feeds install -a
+4. make -j$(nproc)
