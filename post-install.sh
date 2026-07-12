b65f36fded4c03fb4518cfa750f32160f2447e16
thmg1413 <82623565+thmg1413@users.noreply.github.com>
2026-07-12 12:11:08 +0800

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,7 @@
 sed -i 's|downloads\.immortalwrt\.org|immortalwrt.kyarucloud.moe|g' /etc/apk/repositories.d/distfeeds.list
 # 配置 OpenWrt-momo
 wget -O - https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/feed.sh | ash
+wget -O - https://github.com/nikkinikki-org/OpenWrt-nikki/raw/refs/heads/main/feed.sh | ash
 # 配置 NaiveProxy (libcronet)
 wget -O /usr/bin/libcronet.so \
   "https://github.com/SagerNet/cronet-go/releases/latest/download/libcronet-linux-arm64.so" \
